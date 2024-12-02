77e01be7609408ce0cf6705543df503e771021ab
Rafael de Luca <deluca1712@gmail.com>
2024-12-01 22:36:08 -0300

    defining variables

---
+++ basic_sintax/variables.py
@@ -0,0 +1,42 @@
+# assing value to variables
+x=17
+x
+print(x)
+
+x,y,z,w=1,2,4,8
+print(x,y,z,w)
+
+# types of variables
+a=True
+type(a)
+f=False
+type(f)
+print(a,f)
+
+type(0.99)
+int(1.99) # 1
+
+b=3
+float(b) # 3.0
+print(b)
+
+# String
+print('Rafael')
+name = 'Rafael';
+print(name)
+
+sal = 2540.20
+print("Salary: R$ " + str(sal))
+
+#backkash is an escape character
+print('Rafael de Luca isn\'t fine')
+print("It\'s cool, isn\'t it?")
+print('Test ' + 'concatened') 
+print('Rafael','de Luca')
+
+# convert integer to string
+m=50
+print(str(m) + " days") # 50 days
+print('Click "HERE"')
+print('Big','Farmes')
+print("Big Farmes")
