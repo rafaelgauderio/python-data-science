cd3bc50d3538f081ea2c476092f91e33bdd4cdb5
Rafael de Luca <deluca1712@gmail.com>
2024-12-12 11:12:27 -0300

    Example for linear regression

---
+++ statistical/linear_regression_price_weight.py
@@ -0,0 +1,21 @@
+import numpy as np;
+import pandas as pd;
+import matplotlib.pyplot as pt
+import statsmodels.api as st
+
+data = pd.read_csv('linear_regression_price_weight.csv');
+
+print(data);
+
+y = data['price']
+x = data['weight']
+k0=st.add_constant(x);
+results = st.OLS(y,k0).fit();
+print(results.summary());
+
+pt.scatter(x,y);
+yRegression = 102200 + 222.9402*x;
+figure = pt.plot(x,yRegression, lw=2, c='orange', label='regression line')
+pt.xlabel('PRICE', fontsize=19)
+pt.ylabel('WEIGHT', fontsize=19)
+pt.show();
